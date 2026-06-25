cebfa13a4b3908c461978cb47922d30f3ab5c40f
Ashwak Ahmad <ishwakahmadmi@gmail.com>
2026-06-25 08:35:30 +0000

    edited packages

--- models/staging/stg_sales_order_items_sk.sql
+++ models/staging/stg_sales_order_items_sk.sql
@@ -1,5 +1,6 @@
+    -- models/staging/stg_sales_order_items.sql
 select
-    {{ dbt_utils.generate_surrogate_key([ 'VBELN' , 'POSNR']) }} as sales_order_item_sk,
+    {{ dbt_utils.generate_surrogate_key(['vbeln', 'posnr']) }} as sales_order_item_sk,
     vbeln as sales_order_number,
     posnr as item_number,
     matnr as material_number,
