653bf9b82f6e74df351b364b40ad4c14899f3f52
Ashwak Ahmad <ishwakahmadmi@gmail.com>
2026-06-25 10:44:33 +0000

    dbt utils dbt_expectation_+test has done

--- models/staging/stg_sales_order_items.sql
+++ models/staging/stg_sales_order_items.sql
@@ -1,12 +1,15 @@
 {{ config(materialized='view') }}
 
+
 select
+    {{ dbt_utils.generate_surrogate_key(['VBAP.vbeln' , 'VBAP.posnr']) }} as sales_order_item_skey,
     VBAP.vbeln          as sales_order_number,
     VBAP.posnr          as item_number,
     VBAP.matnr          as material_number,
     VBAP.arktx          as material_description,
     VBAP.kwmeng         as order_quantity,
     VBAP.netpr          as item_price,
+    VBAK.netwr          as net_value,
     VBAK.erdat          as order_date,
     VBAK.auart          as order_type,
     VBAK.kunnr          as customer_number,
